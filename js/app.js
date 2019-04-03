9853a30779d226b81d014f62b885e5a952c9a58e
Dmitry Mitskevich <artur.pirogkov.minsk@gmail.com>
2019-04-03 23:28:24 +0300

    Added SortBy to localStorage

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 var lpApp = angular.module('lpApp', []);
 
 lpApp.controller('lpPriceCtrl', function ($scope, $http) {
-
-    $scope.sortBy = 'name';
-    $scope.sortRev = false;
-
+    
     $http.get('price.json').then(function (res) {
         $scope.prices = res.data;
         $scope.calc();
+        $scope.sortGet();
     }).catch(function (err) {
         $scope.reqStatus = err.status;
         $scope.reqStatusText = err.statusText;
@@ -18,8 +16,20 @@ lpApp.controller('lpPriceCtrl', function ($scope, $http) {
             $scope.sortRev = !$scope.sortRev;
         }
         $scope.sortBy = propertyName;
+        localStorage.sortBy = $scope.sortBy;
+        localStorage.sortRev = $scope.sortRev;
     }
-    
+
+    $scope.sortGet = function () {
+        if (localStorage.sortBy && localStorage.sortRev) {
+            $scope.sortBy = localStorage.sortBy;
+            $scope.sortRev = localStorage.sortRev == 'true';
+        } else {
+            $scope.sortBy = 'name';
+            $scope.sortRev = false;
+        }
+    }
+
     $scope.calc = function() {
         $scope.prices.forEach(function(price) {
             price.price2 = price.price * (1 - price.discount);
